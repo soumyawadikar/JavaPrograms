ac11ecbdc8782c7efb5f4647244992c14ba34d7e
soumyawadikar <soumya.jahagirdar@excelr.com>
2024-03-20 17:23:12 +0530

    First Commit..

---
+++ JavaPrograms/src/ReverseString.java
@@ -0,0 +1,14 @@
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+
+  String s= "soumya";
+  char ch[]=s.toCharArray();
+  for(int  i=ch.length-1;i>=0;i--) {
+	 System.out.println(ch[i]);
+  }
+     
+	}
+
+}
